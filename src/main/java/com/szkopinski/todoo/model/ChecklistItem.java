eab0b5913d0b83e7ea5134ab2d2b2dc5747dddd8
Tomek Szkopinski <tszkopinski@gmail.com>
2019-04-18 23:26:33 +0200

    Adds ChecklistItem class

--- src/main/java/com/szkopinski/todoo/model/ChecklistItem.java
+++ src/main/java/com/szkopinski/todoo/model/ChecklistItem.java
@@ -2,9 +2,6 @@ package com.szkopinski.todoo.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,25 +16,16 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@ApiModel(value = "Task model")
 @JsonIgnoreProperties(value = {"id"}, allowGetters = true)
-public class Task {
-
+public class ChecklistItem {
   @Id
   @JsonProperty("id")
   @GeneratedValue(strategy = GenerationType.AUTO)
-  @ApiModelProperty(value = "Id of given task", dataType = "Integer")
   private int id;
 
-  @NotNull(message = "contents is a required field")
-  @Size(min = 1, max = 1024, message = "contents cannot be shorter than 1 and longer than 1024 characters")
-  @ApiModelProperty(value = "Text contents of given task", required = true, dataType = "String", example = "Take dog for a walk.")
-  private String contents;
+  @NotNull(message = "description is a required field")
+  @Size(min = 1, max = 1024, message = "description cannot be shorter than 1 and longer than 1024 characters")
+  private String description;
 
-  @ApiModelProperty(name = "completed", dataType = "boolean")
   private boolean completed;
-
-  //TODO - add swagger and hibernate annotations
-  private List<ChecklistItem> checklist;
-
 }
