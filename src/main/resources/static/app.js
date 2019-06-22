bff5a74d061c53eb318c21a96b81e23d786027ac
Tomek Szkopinski <tszkopinski@gmail.com>
2019-06-22 20:59:08 +0200

    Improves code formatting.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,304 +1,260 @@
 var app = angular.module("todooApp", ["ngAnimate", "ngRoute", "ngFileUpload"]);
 var baseUrl = "/api";
 
-app.config(function($routeProvider) {
+app.config(function ($routeProvider) {
 
-	$routeProvider
-		.when('/tasks', {
-			templateUrl: 'views/tasks.html',
-			controller: 'taskController'
-		})
-		.when('/account', {
-			templateUrl: 'views/account.html',
-			controller: 'accountController'
-		})
-		.when('/about', {
-			templateUrl: 'views/about.html'
-		})
-    .otherwise({
-			redirectTo: '/tasks'
-		});
+    $routeProvider
+        .when('/tasks', {
+            templateUrl: 'views/tasks.html',
+            controller: 'taskController'
+        })
+        .when('/account', {
+            templateUrl: 'views/account.html',
+            controller: 'accountController'
+        })
+        .when('/about', {
+            templateUrl: 'views/about.html'
+        })
+        .otherwise({
+            redirectTo: '/tasks'
+        });
 });
 
-//app.directive('fileModel', ['$parse', function ($parse) {
-//    return {
-//       restrict: 'A',
-//       link: function(scope, element, attrs) {
-//          var model = $parse(attrs.fileModel);
-//          var modelSetter = model.assign;
-//
-//          element.bind('change', function(){
-//             scope.$apply(function(){
-//                modelSetter(scope, element[0].file);
-//             });
-//          });
-//       }
-//    };
-//
-//}]);
+app.controller("taskController", function ($rootScope, $scope, $http) {
 
-app.controller("taskController", function($rootScope, $scope, $http) {
+    getUserName();
 
-  getUserName();
-
-  function getUserName() {
+    function getUserName() {
         $http({
-          method: "GET",
-          url: baseUrl + "/user"
+            method: "GET",
+            url: baseUrl + "/user"
         }).then(
-          function success(response) {
-            $rootScope.username = response.data.name;
-            getAllItems($rootScope.username);
-          },
-          function error(response) {
-            alert(response.statusText);
-          }
-        );
-  }
-
-  function getAllItems(username) {
-
-    $http({
-      method: "GET",
-      url: baseUrl + "/" + username + "/tasks"
-    }).then(
-      function success(response) {
-        $scope.tasks = response.data;
-      },
-      function error(response) {
-        alert(response.statusText);
-      }
-    );
-  }
-
-  $scope.addTask = function() {
-    $http({
-      method: "POST",
-      url: baseUrl + "/tasks",
-      data: {
-        checklist: [],
-        completed: false,
-        contents: $scope.taskToAdd,
-        creationDate: new Date(),
-        deadline: "2019-04-22",
-        userName: $scope.username
-      }
-    }).then(
-      function success() {
-        getAllItems($rootScope.username);
-      },
-      function error(response) {
-        alert(response.statusText);
-      }
-    );
-    clearTodoField();
-  }
-
-  function clearTodoField() {
-    $scope.taskToAdd = "";
-  }
-
-  $scope.markTask = function(x) {
-    !$scope.tasks[x].completed;
-    $http({
-      method: "PUT",
-      url: baseUrl + "/tasks/" + $scope.tasks[x].id,
-      data: {
-        checklist: $scope.tasks[x].checklist,
-        completed: $scope.tasks[x].completed,
-        contents: $scope.tasks[x].contents,
-        creationDate: $scope.tasks[x].creationDate,
-        deadline: $scope.tasks[x].deadline,
-        userName: $scope.tasks[x].userName
-      }
-    }).then(
-      function success() {
-        getAllItems($rootScope.username)
-      },
-      function error(response) {
-        alert(response.statusText)
-      }
-    );
-  };
-
-    $scope.addChecklistItem = function(parentIndex) {
-      $scope.tasks[parentIndex].checklist.push({
-        description: "New item",
-        completed: "false"
-      });
-      $scope.updateTask(parentIndex);
-    }
-
-    $scope.updateTask = function(x) {
-      $http({
-        method: "PUT",
-        url: baseUrl + "/tasks/" + $scope.tasks[x].id,
-        data: {
-          checklist: $scope.tasks[x].checklist,
-          completed: $scope.tasks[x].completed,
-          contents: $scope.tasks[x].contents,
-          creationDate: $scope.tasks[x].creationDate,
-          deadline: $scope.tasks[x].deadline,
-          userName: $scope.tasks[x].userName
-        }
-      }).then(
-        function success() {
-          getAllItems($rootScope.username);
-        },
-        function error(response) {
-          alert(response.statusText);
-        }
-      );
-    };
-
-    $scope.deleteChecklistItem = function(parentIndex, index) {
-      $scope.tasks[parentIndex].checklist.splice(index, 1);
-      $scope.updateTask(parentIndex);
-    }
-
-    $scope.markChecklistItem = function(parentIndex, index) {
-      !$scope.tasks[parentIndex].checklist[index].completed;
-      $http({
-        method: "PUT",
-        url: baseUrl + "/tasks/" + $scope.tasks[parentIndex].id,
-        data: {
-         checklist: $scope.tasks[parentIndex].checklist,
-         completed: $scope.tasks[parentIndex].completed,
-         contents: $scope.tasks[parentIndex].contents,
-         creationDate: $scope.tasks[parentIndex].creationDate,
-         deadline: $scope.tasks[parentIndex].deadline,
-         userName: $scope.tasks[parentIndex].userName
-         }
-      }).then(
-        function success() {
-          getAllItems($rootScope.username);
-        },
-        function error(response) {
-          alert(response.statusText);
-        }
-      );
-    };
-
-  $scope.deleteTask = function(x) {
-    $http({
-      method: "DELETE",
-      url: baseUrl + "/tasks/" + $scope.tasks[x].id
-    }).then(
-      function success() {
-        getAllItems($rootScope.username);
-      },
-      function error(response) {
-        alert(response.statusText);
-      }
-    );
-  };
-
-  $scope.logout = function() {
-    $http.post("/logout", {})
-    .then(function success() {
-    window.location.href = "/";
-    },
-    function error(response) {
-      alert(response.statusText);
-     }
-    );
-  }
-});
-
-app.controller("securityController", function($scope, $http) {
-
-  $scope.credentials = {};
-
-  $scope.login = function() {
-    $http({
-    method: "GET",
-    url: baseUrl + "/user",
-    headers: {"Authorization": "Basic " + btoa($scope.credentials.username + ":" + $scope.credentials.password)
-    }}).then(
-             function success(response) {
-               window.location.href = "/index.html";
-             },
-             function error(response) {
-               alert(response.statusText);
-             }
-           );
-         }
-});
-
-app.controller("accountController", function($rootScope, $scope, $http, Upload, $timeout) {
-
-  getAccount();
-
-  $scope.uploadFiles = function(file, errFiles) {
-          $scope.f = file;
-          $scope.errFile = errFiles && errFiles[0];
-          if (file) {
-              file.upload = Upload.upload({
-                  url: baseUrl + '/images/uploadFile',
-                  data: {file: file}
-              });
-
-              file.upload.then(function (response) {
-                  $timeout(function () {
-                      file.result = response.data;
-                      $scope.avatarUrl = file.result.fileDownloadUri;
-                  });
-              }, function (response) {
-                  if (response.status > 0)
-                      $scope.errorMsg = response.status + ': ' + response.data;
-              }, function (evt) {
-                  file.progress = Math.min(100, parseInt(100.0 *
-                                           evt.loaded / evt.total));
-              });
-          }
-      }
-
-//  $scope.uploadResult = "";
-
-//  $scope.myForm = {
-//          file: null
-//  }
-
-  function getAccount() {
-        $http({
-          method: "GET",
-          url: baseUrl + "/accounts/" + $rootScope.username
-        }).then(
-          function success(response) {
-            $scope.accountData = response.data;
-            if ($scope.accountData.avatar) {
-            $scope.avatarUrl = baseUrl + "/images/downloadFile/" + $scope.accountData.avatar.id;
-            } else {
-            $scope.avatarUrl = "images/default_avatar.png";
+            function success(response) {
+                $rootScope.username = response.data.name;
+                getAllItems($rootScope.username);
+            },
+            function error(response) {
+                alert(response.statusText);
             }
-            console.log($scope.accountData);
-          },
-          function error(response) {
-            alert(response.statusText);
-          }
         );
-  }
+    }
 
-//  $scope.doUploadFile = function() {
-//
-//          var url = baseUrl + "/images/uploadFile";
-//          var data = new FormData();
-//          data.append("file", $scope.myForm.file);
-//
-//          var config = {
-//              transformRequest: angular.identity,
-//              transformResponse: angular.identity,
-//              headers: {
-//                  'Content-Type': undefined
-//              }
-//          }
-//
-//          $http.post(url, data, config).then(
-//              function(response) {
-//                  $scope.uploadResult = response.data;
-//              },
-//              function(response) {
-//                  $scope.uploadResult = response.data;
-//              });
-//      };
+    function getAllItems(username) {
+
+        $http({
+            method: "GET",
+            url: baseUrl + "/" + username + "/tasks"
+        }).then(
+            function success(response) {
+                $scope.tasks = response.data;
+            },
+            function error(response) {
+                alert(response.statusText);
+            }
+        );
+    }
+
+    $scope.addTask = function () {
+        $http({
+            method: "POST",
+            url: baseUrl + "/tasks",
+            data: {
+                checklist: [],
+                completed: false,
+                contents: $scope.taskToAdd,
+                creationDate: new Date(),
+                deadline: "2019-04-22",
+                userName: $scope.username
+            }
+        }).then(
+            function success() {
+                getAllItems($rootScope.username);
+            },
+            function error(response) {
+                alert(response.statusText);
+            }
+        );
+        clearTodoField();
+    };
+
+    function clearTodoField() {
+        $scope.taskToAdd = "";
+    }
+
+    $scope.markTask = function (x) {
+        !$scope.tasks[x].completed;
+        $http({
+            method: "PUT",
+            url: baseUrl + "/tasks/" + $scope.tasks[x].id,
+            data: {
+                checklist: $scope.tasks[x].checklist,
+                completed: $scope.tasks[x].completed,
+                contents: $scope.tasks[x].contents,
+                creationDate: $scope.tasks[x].creationDate,
+                deadline: $scope.tasks[x].deadline,
+                userName: $scope.tasks[x].userName
+            }
+        }).then(
+            function success() {
+                getAllItems($rootScope.username)
+            },
+            function error(response) {
+                alert(response.statusText)
+            }
+        );
+    };
+
+    $scope.addChecklistItem = function (parentIndex) {
+        $scope.tasks[parentIndex].checklist.push({
+            description: "New item",
+            completed: "false"
+        });
+        $scope.updateTask(parentIndex);
+    };
+
+    $scope.updateTask = function (x) {
+        $http({
+            method: "PUT",
+            url: baseUrl + "/tasks/" + $scope.tasks[x].id,
+            data: {
+                checklist: $scope.tasks[x].checklist,
+                completed: $scope.tasks[x].completed,
+                contents: $scope.tasks[x].contents,
+                creationDate: $scope.tasks[x].creationDate,
+                deadline: $scope.tasks[x].deadline,
+                userName: $scope.tasks[x].userName
+            }
+        }).then(
+            function success() {
+                getAllItems($rootScope.username);
+            },
+            function error(response) {
+                alert(response.statusText);
+            }
+        );
+    };
+
+    $scope.deleteChecklistItem = function (parentIndex, index) {
+        $scope.tasks[parentIndex].checklist.splice(index, 1);
+        $scope.updateTask(parentIndex);
+    };
+
+    $scope.markChecklistItem = function (parentIndex, index) {
+        !$scope.tasks[parentIndex].checklist[index].completed;
+        $http({
+            method: "PUT",
+            url: baseUrl + "/tasks/" + $scope.tasks[parentIndex].id,
+            data: {
+                checklist: $scope.tasks[parentIndex].checklist,
+                completed: $scope.tasks[parentIndex].completed,
+                contents: $scope.tasks[parentIndex].contents,
+                creationDate: $scope.tasks[parentIndex].creationDate,
+                deadline: $scope.tasks[parentIndex].deadline,
+                userName: $scope.tasks[parentIndex].userName
+            }
+        }).then(
+            function success() {
+                getAllItems($rootScope.username);
+            },
+            function error(response) {
+                alert(response.statusText);
+            }
+        );
+    };
+
+    $scope.deleteTask = function (x) {
+        $http({
+            method: "DELETE",
+            url: baseUrl + "/tasks/" + $scope.tasks[x].id
+        }).then(
+            function success() {
+                getAllItems($rootScope.username);
+            },
+            function error(response) {
+                alert(response.statusText);
+            }
+        );
+    };
+
+    $scope.logout = function () {
+        $http.post("/logout", {})
+            .then(function success() {
+                    window.location.href = "/";
+                },
+                function error(response) {
+                    alert(response.statusText);
+                }
+            );
+    }
+});
+
+app.controller("securityController", function ($scope, $http) {
+
+    $scope.credentials = {};
+
+    $scope.login = function () {
+        $http({
+            method: "GET",
+            url: baseUrl + "/user",
+            headers: {
+                "Authorization": "Basic " + btoa($scope.credentials.username + ":" + $scope.credentials.password)
+            }
+        }).then(
+            function success() {
+                window.location.href = "/index.html";
+            },
+            function error(response) {
+                alert(response.statusText);
+            }
+        );
+    }
+});
+
+app.controller("accountController", function ($rootScope, $scope, $http, Upload, $timeout) {
+
+    getAccount();
+
+    $scope.uploadFiles = function (file, errFiles) {
+        $scope.f = file;
+        $scope.errFile = errFiles && errFiles[0];
+        if (file) {
+            file.upload = Upload.upload({
+                url: baseUrl + '/images/uploadFile',
+                data: {file: file}
+            });
+
+            file.upload.then(function (response) {
+                $timeout(function () {
+                    file.result = response.data;
+                    $scope.avatarUrl = file.result.fileDownloadUri;
+                });
+            }, function (response) {
+                if (response.status > 0)
+                    $scope.errorMsg = response.status + ': ' + response.data;
+            }, function (evt) {
+                file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+            });
+        }
+    };
+
+    function getAccount() {
+        $http({
+            method: "GET",
+            url: baseUrl + "/accounts/" + $rootScope.username
+        }).then(
+            function success(response) {
+                $scope.accountData = response.data;
+                if ($scope.accountData.avatar) {
+                    $scope.avatarUrl = baseUrl + "/images/downloadFile/" + $scope.accountData.avatar.id;
+                } else {
+                    $scope.avatarUrl = "images/default_avatar.png";
+                }
+                console.log($scope.accountData);
+            },
+            function error(response) {
+                alert(response.statusText);
+            }
+        );
+    }
+
 });
--- src/test/java/com/szkopinski/todoo/controller/TaskControllerTest.java
+++ src/test/java/com/szkopinski/todoo/controller/TaskControllerTest.java
@@ -1,25 +1,9 @@
 package com.szkopinski.todoo.controller;
 
-import static com.szkopinski.todoo.helpers.TestHelpers.convertToJson;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.sun.tools.javac.util.List;
 import com.szkopinski.todoo.model.Task;
 import com.szkopinski.todoo.model.UserName;
 import com.szkopinski.todoo.repository.AccountRepository;
 import com.szkopinski.todoo.service.TaskService;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +13,18 @@ import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static com.szkopinski.todoo.helpers.TestHelpers.convertToJson;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = TaskController.class)
 class TaskControllerTest {
